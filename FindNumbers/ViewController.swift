7a0a79497ce79751d8cae8fd61162c1ef05841a1
Roman Postrigaylo <bbromka27@gmail.com>
2023-02-25 16:58:15 +0500

    Initial Commit

---
+++ FindNumbers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindNumbers
+//
+//  Created by Роман Постригайло on 25.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
